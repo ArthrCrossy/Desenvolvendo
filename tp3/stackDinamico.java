bbf20aacce15d373ef2efa63d0893888971d9daf
ArthrCrossy <68568367+ArthrCrossy@users.noreply.github.com>
2022-02-03 11:23:33 -0300

    Add files via upload

---
+++ tp3/stackDinamico.java
@@ -0,0 +1,665 @@
+class Filmes implements Cloneable {
+
+    // declarando variaveis
+    private String nome;
+    private String Formato;
+    private String Duracao;
+    private String pais;
+    private String idioma;
+    private String Emissora;
+    private String transmissao;
+    private int numberTemp;
+    private int numberEp;
+    
+    // declarando construtor
+    public Filmes(){
+        
+    }
+
+    // print
+    public String toString(){
+
+        return this.nome + " " + this.Formato + " " + this.Duracao + " " + this.pais + " " + this.idioma + " " + this.Emissora + " " + this.transmissao + " " + this.numberTemp + " " + this.numberEp;
+    }
+
+    // construtura jova
+    public Filmes(String nome, String Formato, String Duracao, String pais, String idioma, String Emissora, String transmissao, int numberTemp, int numberEp){
+        this.Formato = Formato;
+        this.Duracao = Duracao;
+        this.pais = pais;
+        this.idioma = idioma;
+        this.Emissora = Emissora;
+        this.transmissao = transmissao;
+        this.numberTemp = numberTemp;
+        this.numberEp = numberEp;
+        
+    }
+
+    // Gets e Sets
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getFormato(){
+        return this.Formato;
+    }
+
+    public String getDuracao(){
+        return this.Duracao;
+    }
+
+    public String getPais(){
+        return this.pais;
+    }
+
+    public String getIdioma(){
+        return this.idioma;
+    }
+
+    public String getEmissora(){
+        return this.Emissora;
+    }
+
+    public String getTransmissao(){
+        return this.transmissao;
+    }
+
+    public int getNumberTemp(){
+        return this.numberTemp;
+    }
+
+    public int getNumberEp(){
+        return this.numberEp;
+    }
+
+    public void SetNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setFormato(String Formato){
+        this.Formato = Formato;
+    }
+
+    public void setDuracao(String Duracao){
+        this.Duracao = Duracao;
+    }
+
+    public void setPais(String pais){
+        this.pais = pais;
+    }
+
+    public void setIdioma(String idioma){
+        this.idioma = idioma;
+    }
+
+    public void setEmissora(String Emissora){
+        this.Emissora = Emissora;
+    }
+
+    public void setTransmissao(String transmissao){
+        this.transmissao = transmissao;
+    }
+
+    public void setNumbertemp(int numberTemp){
+        this.numberTemp = numberTemp;
+    }
+
+    public void setNumberEp(int numberEp){
+        this.numberEp = numberEp;
+    }
+
+    // Clone
+    Filmes getClone(){
+        try {
+            return (Filmes) super.clone();
+        } catch (CloneNotSupportedException e) {
+            System.out.println("ERRO CLONE");
+            return this;
+        }
+    }
+
+    public void leFilme(String nomeArquivo){
+
+        String linhaFormato = "";
+        String linhaTemporada = "";
+        String novaTemporada = "";
+        String linhaOrigem = "";
+        String novoOrigem = "";
+        String novoFormato = "";
+        String linhaEmissora = "";
+        String linhaIdioma = "";
+        String novoIdioma = "";
+        String novaEmissora = "";
+        String linhaEpisodio = "";
+        String novoEpisodio = "";
+        String linhaTransmissao = "";
+        String novaTransmissao = "";
+        String linhaDuracao = "";
+        String novaDuracao = "";
+        String linhaTitulo = "";
+        String novoTitulo = "";
+        
+        int i =0;
+
+        // concatena o string
+          String x = "/tmp/series/";
+          x += nomeArquivo;
+         // Arq.openRead(nomeArquivo, "UTF-8");      
+          Arq.openRead(x, "UTF-8");
+
+        //Descobre a posição no site
+        for(i = 0; i <5; i = i + 1){
+            linhaTitulo = Arq.readLine();
+        }
+        
+        linhaTitulo = limpaDados(linhaTitulo);
+        novoTitulo = limpaTitulo(linhaTitulo);
+        SetNome(novoTitulo);
+
+        //Descobre a posição no site
+        for(i =0; Arq.hasNext() == true; i = i + 1){
+            
+            if(linhaFormato.contains("Formato")){
+               
+               break;
+            }
+            linhaFormato = Arq.readLine();
+        }
+
+        
+        for(i = 0; i <1; i = i + 1){
+            linhaFormato = Arq.readLine();
+            novoFormato = limpaDados(linhaFormato);
+
+        }
+
+        setFormato(novoFormato);
+
+        
+        //Descobre a posição no site
+        for(i =0; Arq.hasNext() == true; i = i + 1){
+            
+            if(linhaDuracao.contains("Duração")){
+                break;
+            }
+            linhaDuracao = Arq.readLine();
+        }
+
+        for(i = 0; i <1; i = i + 1){
+            linhaDuracao = Arq.readLine();
+            novaDuracao = limpaDados(linhaDuracao);
+
+        }
+
+        setDuracao(novaDuracao);
+
+        //Descobre a posição no site
+        for(i =0; Arq.hasNext() == true; i = i + 1){
+            
+            if(linhaOrigem.contains("País de origem")){
+                break;
+            }
+            linhaOrigem = Arq.readLine();
+        }
+
+        //Descobre a posição no site
+        for(i = 0; i <1; i = i + 1){
+            linhaOrigem = Arq.readLine();
+            novoOrigem = limpaDados(linhaOrigem);
+
+        }
+
+        setPais(novoOrigem);
+
+        //Descobre a posição no site
+        for(i =0; Arq.hasNext() == true; i = i + 1){
+            
+            if(linhaIdioma.contains("Idioma original")){
+                break;
+            }
+            linhaIdioma = Arq.readLine();
+        }
+
+        for(i = 0; i <1; i = i + 1){
+            linhaIdioma = Arq.readLine();
+            novoIdioma = limpaDados(linhaIdioma);
+
+        }
+        
+        setIdioma(novoIdioma);
+
+        for(i =0; Arq.hasNext() == true; i = i + 1){
+            
+            if(linhaEmissora.contains("Emissora")){
+                break;
+            }
+            linhaEmissora = Arq.readLine();
+        }
+
+        for(i = 0; i <1; i = i + 1){
+            linhaEmissora = Arq.readLine();
+            novaEmissora = limpaDados(linhaEmissora);
+        }
+
+        setEmissora(novaEmissora);
+
+        //Descobre a posição no site
+        for(i =0; Arq.hasNext() == true; i = i + 1){
+            
+            if(linhaTransmissao.contains("Transmissão original")){
+                break;
+            }
+            linhaTransmissao = Arq.readLine();
+        }
+
+        for(i = 0; i <1; i = i + 1){
+            linhaTransmissao = Arq.readLine();
+            linhaTransmissao = firstDataTreat(linhaTransmissao);
+            novaTransmissao = limpaDadosData(linhaTransmissao);
+        }
+
+        setTransmissao(novaTransmissao);
+        
+        //Descobre a posição no site
+        for(i =0; Arq.hasNext() == true; i = i + 1){
+            
+            if(linhaTemporada.contains("N.º de temporadas")){
+                break;
+            }
+            linhaTemporada = Arq.readLine();
+        }
+
+        for(i = 0; i <1; i = i + 1){
+            linhaTemporada = Arq.readLine();
+            novaTemporada = limpaDados(linhaTemporada);
+            novaTemporada = limpastringTemp(novaTemporada);
+            
+        }
+
+        //Descobre a posição no site
+        setNumbertemp(Integer.parseInt(novaTemporada));
+
+        for(i =0; Arq.hasNext() == true; i = i + 1){
+            
+            if(linhaEpisodio.contains("N.º de episódios")){
+                break;
+            }
+            linhaEpisodio = Arq.readLine();
+        }
+
+        for(i = 0; i <1; i = i + 1){
+            
+            linhaEpisodio = Arq.readLine();
+            novoEpisodio = limpaDados(linhaEpisodio);
+            novoEpisodio = limpaListaEpisodios(novoEpisodio);
+            novoEpisodio = limpastringEp(novoEpisodio);
+           
+        }
+
+        setNumberEp(Integer.parseInt(novoEpisodio));
+
+//        MyIO.println(toString());
+        Arq.close();
+
+    }
+
+    // numeros
+    public String limpastringEp(String s){
+
+        String novoS = "";
+        int tam = s.length();
+        
+        for(int i  = 0; i < tam; i = i +1){
+        novoS =  novoS + s.charAt(i);
+        }
+
+        novoS = novoS.trim();
+
+        return novoS;
+    }
+
+        // limpa as temporadas
+    public String limpaListaEpisodios(String ep){
+
+        int tam = ep.length();
+        String newEp = "";
+        boolean continuar = true;
+
+        for(int i = 0; i < tam && continuar == true; i = i + 1){
+            
+            // apenas numneros
+            if(ep.charAt(i) >= 48 && ep.charAt(i)<= 57){
+                newEp = newEp + ep.charAt(i);
+            }
+            // quebra c tiver string
+            if(ep.charAt(i) == '+' || ep.charAt(i) == '-'  || ep.charAt(i) == ' '){
+                continuar = false;
+                
+            }
+
+        }
+
+        return newEp;
+    }
+
+    // limpa as temporadas
+    public String limpastringTemp(String ep){
+
+        int tam = ep.length();
+        String newEp = "";
+        boolean continuar = true;
+
+        for(int i = 0; i < tam && continuar == true; i = i + 1){
+
+            // Apenas numeros
+            if(ep.charAt(i) >= 48 && ep.charAt(i)<= 57){
+                newEp = newEp + ep.charAt(i);
+            }
+            // quebra c tiver string
+            if(ep.charAt(i) == '+' || ep.charAt(i) == '-'  || ep.charAt(i) == ' '){
+                continuar = false;
+                
+            }
+
+        }
+
+        //newEp = newEp.trim();
+
+        return newEp;
+    }
+
+    // limpa o titulo 
+    public String limpaTitulo(String palavra){
+
+        String novaPalavra = "";
+        int tam = palavra.length();
+        boolean ehTrue = true;
+
+
+        for(int i = 0; i < tam && ehTrue; i = i + 1){
+
+            if(palavra.charAt(i) == '–' || palavra.charAt(i) == '('){
+
+                ehTrue = false;
+            }else{
+                novaPalavra = novaPalavra + palavra.charAt(i);
+            }
+
+
+        }
+
+        novaPalavra = novaPalavra.trim();
+
+
+        return novaPalavra;
+    }
+
+
+    // consegue limpar conteúdo entre < e > e &
+    // e adicionar em novaPalavra
+    public String limpaDados(String palavra){
+
+        String novaPalavra = "";
+        int tam = palavra.length();
+
+        for(int i = 0; i < tam; i = i + 1){
+            if(palavra.charAt(i) == '<' || palavra.charAt(i) == '&'){
+                for(int y = i; y < tam; y = y + 1){
+                    if(palavra.charAt(y) == '>'){
+                        
+                        i = y;
+                        y = tam;
+                    }
+                }
+            }else{
+
+                novaPalavra = novaPalavra + palavra.charAt(i);
+            }
+        }
+
+        novaPalavra = novaPalavra.trim();
+
+
+        return novaPalavra;
+    }
+
+    // retira &#160;
+    public String firstDataTreat(String palavra){
+        /*&#160;*/
+        
+        String novaPalavra = palavra.replace("&#160;", "");
+
+        return novaPalavra;
+    }
+
+    // consegue limpar conteúdo entre < e > e &
+    // e adicionar em novaPalavra
+    public String limpaDadosData(String palavra){
+
+        String novaPalavra = "";
+        int tam = palavra.length();
+
+        for(int i = 0; i < tam; i = i + 1){
+            if(palavra.charAt(i) == '<' || palavra.charAt(i) == '&'){
+                for(int y = i; y < tam; y = y + 1){
+                    if(palavra.charAt(y) == '>'){
+                        i = y;
+                        y = tam;
+                    }
+                }
+            }else{
+
+                novaPalavra = novaPalavra + palavra.charAt(i);
+            }
+        }
+
+        novaPalavra = novaPalavra.trim();
+
+        return novaPalavra;
+    }
+
+}
+
+// classe Pilha
+
+class celula{
+
+    celula prox;
+    Filmes elemento;
+
+    celula(Filmes x){
+        elemento = x;
+        x = null;
+    }
+}
+
+class pilha {
+
+	celula top;
+
+	public pilha() {
+		top = null;
+	}
+
+	public void inserir(Filmes x) {
+
+		celula aux = new celula(x);
+		aux.prox = top;
+		top = aux;
+		aux = null;
+	}
+
+	public String remover() throws Exception {
+
+		if (top == null) {
+			throw new Exception("Erro ao remover!");
+		}
+        
+		Filmes resp = top.elemento;
+		celula aux = top;
+		top = top.prox;
+		aux.prox = null;
+		aux = null;
+
+		return resp.getNome();
+	}   
+
+	public void mostrar() {
+
+		for (celula i = top; i != null; i = i.prox) {
+			MyIO.println(i.elemento.getNome() + " " + i.elemento.getFormato() + " " + i.elemento.getDuracao() + " " + i.elemento.getPais() + " " + i.elemento.getIdioma() + " " + i.elemento.getEmissora() + " " + i.elemento.getTransmissao() + " " + i.elemento.getNumberTemp() + " " + i.elemento.getNumberEp());
+		}
+	}
+
+
+}
+ 
+public class stack {
+
+    // Visualiza o fim dos sites
+   
+    public static boolean ehFim(String s){
+        return(s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    // Transforma String em vetor de Char
+    public static char[] transformaChar(String s){
+
+        char[] palavra = new char[s.length()];
+
+        for(int i = 0; i < s.length(); i = i + 1){
+            palavra[i] = s.charAt(i);
+        }
+
+        return palavra;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        MyIO.setCharset("UTF-8");
+
+        rodar();
+
+    }
+
+    // quebra String pegar só o html
+    public static String transformaString(String s){
+
+        String newPalavra = new String();
+
+        for(int i = 2; i < s.length(); i = i + 1){
+
+            newPalavra = newPalavra + s.charAt(i) ;
+
+        }
+
+        return newPalavra;
+    }
+
+    public static String transformarStringpespaco(String s){
+
+        String newPalavra = new String();
+
+        for(int i = 6; i < s.length(); i = i + 1){
+
+            if(s.charAt(i) != ' ' || s.charAt(i) != 240){
+
+                newPalavra = newPalavra + s.charAt(i);
+
+            }
+
+        }
+
+        return newPalavra;
+    }
+
+    // Transforma numero em String
+    public static String transformarNumero(String s){
+
+        String palavra = new String();
+        int cont = 0;
+        boolean ok = true;
+
+        for(int i = 0; i < s.length() && ok == true; i = i + 1){
+
+            if(s.charAt(i) >= 48 && s.charAt(i) <= 57){
+
+                palavra = palavra + s.charAt(i);
+                cont = cont + 1;
+
+                if(cont == 2){
+                    ok = false;
+                }
+            }
+        }
+
+        return palavra;
+    }
+
+    // transforma o vetor de string para uma pilha de objeto
+    public static pilha preencheVetor(String input[], int n, pilha x) throws Exception {
+
+        Filmes[] vetor = new Filmes[100];
+
+        for(int i = 0; i < n; i = i + 1){
+            vetor[i] = new Filmes();
+            vetor[i].leFilme(input[i]);
+            x.inserir(vetor[i]);
+        }
+
+        return x;
+    }
+
+    // insere ou remove
+    public static void verifica(pilha x) throws Exception{
+
+        int tam = MyIO.readInt();
+        String palavra = new String();
+
+        while(tam > 0){
+
+            Filmes html = new Filmes();
+            palavra = MyIO.readLine();
+
+            if(palavra.charAt(0) == 'I'){
+
+                palavra = transformaString(palavra);
+                html.leFilme(palavra);
+                x.inserir(html);
+            }
+
+            if(palavra.charAt(0) == 'R'){
+
+            MyIO.println("(R) " + x.remover());
+            }
+
+            tam = tam - 1;
+        }
+
+
+    }
+
+    public static void rodar() throws Exception{
+
+        int i = -1;
+
+        String[] input = new String[100];
+
+        pilha x = new pilha();
+
+        do{
+             i = i + 1;
+            input[i] = MyIO.readLine();
+
+        }while(!ehFim(input[i]));
+        
+        x = preencheVetor(input, i, x);
+
+        verifica(x);
+        
+        x.mostrar();
+        
+    }
+
+}
+
